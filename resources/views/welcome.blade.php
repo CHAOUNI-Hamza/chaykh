a9decf9bcef261bb1ba8e11fc8405cfa22d9d767
hamza chaouni <chaounihamza19@gmail.com>
2022-03-10 15:41:34 -0800

    gdtt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('visitor/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('visitor/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ secure_asset('visitor/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ secure_asset('visitor/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('visitor/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('visitor/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('visitor/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('visitor/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('visitor/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('visitor/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('visitor/assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('visitor/assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: MeFamily - v4.7.0
@@ -85,14 +85,14 @@
   </footer><!-- End Footer -->
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('visitor/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('visitor/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('visitor/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('visitor/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('visitor/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('visitor/assets/js/main.js') }}"></script>
+  <script src="{{ secure_asset('visitor/assets/js/main.js') }}"></script>
 
 </body>
 
