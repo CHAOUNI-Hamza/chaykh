98b8836c56e8b13fb1e9b854adc4df80628f7719
hamza chaouni <chaounihamza19@gmail.com>
2022-03-11 12:27:42 -0800

    gdttffsddsdfsgsd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,7 +95,7 @@
           <div class="swiper-wrapper align-items-center">
           @foreach ($images as $image)
             <div class="swiper-slide"><a href="assets/img/recent-photos/recent-photos-1.jpg" class="glightbox">
-              <img src="{{ Voyager::image( $image->photo ) }}" class="img-fluid" alt="">
+              <img src="$image->photo" class="img-fluid" alt="">
               <!--<img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2021%2F04%2F15%2FGettyImages-512083847-1-2000.jpg" class="img-fluid" alt="">-->
             </a></div>
             @endforeach
